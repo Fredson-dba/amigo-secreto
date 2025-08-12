0e8140b263093ac1db0826a7ba423aceae24ca2e
Fredson-dba <fredson.magalhaes@gmail.com>
2025-08-11 21:52:12 -0300

    implementada a função de sortear amigo e adicionar ao clicar no "enter"

--- app.js
+++ app.js
@@ -3,30 +3,35 @@ let amigos = [];
 
 //Implementando função para adicionar os amigos à lista
 function adicionarAmigo() {
-    let input = document.getElementById('amigo'); // capturar o valor no campo de entrada
-    let nome = input.value.trim(); //remove os espaços em branco no início e no final de uma string
+    let nome = document.getElementById('amigo').value; // capturar o valor no campo de entrada
+    let espacoNome = nome.trim(); //remove os espaços em branco no início e no final do nome 
 
 //Validar a entrada para que o campo não esteja vazio.
     if (nome == '') {
         alert ('Você esqueceu de informar o nome do amigo. Favor, digitar o nome do amigo'); // Se estiver vazio, retorna mensagem de erro
         return;
-    } else {
-      
+    }
+ 
+//Verificar se o nome já foi adicionado
+    if (amigos.includes(nome)) {
+        alert('Este nome já consta na lista, Favor, adicione outro amigo');
+        return;
+    }
+
     amigos.push(nome); //adicionar nome do amigo;
     atualizarListaDeAmigos(); //Atualizar lista de amigos na tela
-    input.value = ''; //Limpar o campo de entrada após adicionar nome do amigo
+    document.getElementById('amigo').value = ''; //Limpar o campo de entrada após adicionar nome do amigo
     }
-}
 
-// Substituir o clique com o mouse no botão Adicionar por teclar "Enter"
+    // Substituir o clique com o mouse no botão Adicionar por teclar "Enter"
    document.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     // Encontre o botão pelo seu ID ou classe
     const button = document.getElementById('botaoAdicionar'); // Substitua 'meuBotao' pelo ID do seu botão
     
     // Verifique se o botão existe antes de tentar clicar
-    if (button) {
-      button.click();
+   if (button) {
+     button.click();
     }
   }
 });
@@ -42,5 +47,35 @@ function atualizarListaDeAmigos() {
         li.textContent = amigos[cont];
         listaDeamigos.appendChild(li); // adicionando um novo nome a lista
     }
+}
+
+//Implementando a função para sortear amigo secreto
+function sortearAmigo() {
+
+//Validando se algum amigo já foi adicionado para o sorteio
+    if (amigos.length === 0) {
+       alert ('você não adicionou nenhum amigo!!!'); 
+       return;
+    
+ } else
+        if (amigos.length < 3) {
+            alert ('Favor, cadastrar 03 amigos para possibilitar o sorteio !!!'); //Verificando se existem no mínimo 03 amigos cadastrados
+            return;
+        } else {
+
+//Selecionar um índice aleatóriamente do array (vetor) amigos
+        let nomeSorteado = Math.floor(Math.random() * amigos.length);
+//Obter o nome sorteado utilizando o índice aleatório
+        let amigoSorteado = amigos[nomeSorteado];
+//Exibir o resultado do sorteio
+let resultadoSorteio = document.getElementById('resultadoSorteio');
+
+resultado.innerHTML = `O amigo sorteado é: <strong>${amigoSorteado}</strong>`;
+
+//Limpa lista de amigos cadastrados
+document.getElementById('listaAmigos').innerHTML = '';
+
+ 
+        }
 }
 
